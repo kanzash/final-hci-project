8258cddcec323779d68d837bacde5eabf56bbf50
kanzash <kanzash@github.com>
2020-12-17 21:31:28 -0500

    the focus tab finally works with timer, doing list now

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
+var myTimer;
+   function clock() {
+     myTimer = setInterval(myClock, 1000);
+     var c = 25;
+
+     function myClock() {
+       document.getElementById("demo").innerHTML = --c;
+       if (c == 0) {
+         clearInterval(myTimer);
+         alert("Reached zero");
+       }
+     }
+   }
+
+
+function disableStopButton() {
+	document.getElementById("stop-timer-button").disabled = "true"
+}
+
+function enableStopButton() {
+	document.getElementById("stop-timer-button").disabled = "false"
+	document.getElementById("stop-timer-button").enabled = "true"
+}
+
+
+function startTimer(duration, display) {
+	var timer = duration, minutes, seconds;
+	document.getElementById("stop-timer-button").disabled = "false"
+	document.getElementById("start-timer-button").disabled = "true"
+	enableStopButton()
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+	}, 1000);
+	
+}
+
 //Document is the DOM can be accessed in the console with document.window.
 // Tree is from the top, html, body, p etc.
 
